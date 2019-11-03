4b9c77a17e38c86016ac96bab379efce3adb73ea
alex <camward@yandex.ru>
2019-11-03 15:41:33 +0400

    добавление задачи (POST)

--- public/app.js
+++ public/app.js
@@ -21,8 +21,6 @@ new Vue({
       })
         .then(res => res.json())
         .then(({todo}) => {
-          console.log(todo)
-
           this.todos.push(todo)
           this.todoTitle = ''
         })
@@ -36,12 +34,19 @@ new Vue({
     capitalize(value) {
       return value.toString().charAt(0).toUpperCase() + value.slice(1)
     },
-    date(value) {
-      return new Intl.DateTimeFormat('ru-RU', {
+    date(value, withTime) {
+      const options = {
         year: 'numeric',
         month: 'long',
         day: '2-digit'
-      }).format(new Date(value))
+      }
+
+      if (withTime) {
+        options.hour = '2-digit'
+        options.minute = '2-digit'
+        options.second = '2-digit'
+      }
+      return new Intl.DateTimeFormat('ru-RU', options).format(new Date(value))
     }
   }
 })
